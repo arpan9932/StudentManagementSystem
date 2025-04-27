80eedb4627981a69f513dcb89ef72f1706061f58
ARPAN BERA <120242028+arpan9932@users.noreply.github.com>
2025-04-27 11:13:38 +0530

    Update Course.java

--- model/Course.java
+++ model/Course.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Course {
+    public String CourseName;
+    public String CourseCode;
+    public Course(String CourseCode,String CourseName){
+        this.CourseCode=CourseCode;
+        this.CourseName=CourseName;
+    }
+    public  String getCourseName(){
+        return  CourseName;
+    }
+    public  String getCourseCode(){
+        return  CourseCode;
+    }
+    @Override
+    public String toString(){
+        return  CourseName +"("+CourseCode+")";
+    }
 }
