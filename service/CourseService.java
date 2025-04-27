d63692a02a543bb4c45a4c571290412a1052be7b
ARPAN BERA <120242028+arpan9932@users.noreply.github.com>
2025-04-27 11:14:52 +0530

    Update CourseService.java

--- service/CourseService.java
+++ service/CourseService.java
@@ -1,4 +1,30 @@
 package service;
+import model.Course;
+import model.Student;
 
+import java.util.*;
 public class CourseService {
+    private  List<Course>courseList=new ArrayList<>();
+
+    public void addCourse(Course course){
+        for(Course c:courseList){
+            if(c.getCourseCode().equalsIgnoreCase(course.getCourseCode())){
+                System.out.println("course code already exits...");
+                return;
+            }
+        }
+        courseList.add(course);
+    }
+    public  Course FindCourseByCode(String  code){
+        for(Course course:courseList){
+            if(course.getCourseCode().equalsIgnoreCase(code)) {
+                return course;
+            }
+        }
+        return  null;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
 }
