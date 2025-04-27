6107eccfbf5cb519f86d1f80cd0e8f4a8965bc78
ARPAN BERA <120242028+arpan9932@users.noreply.github.com>
2025-04-27 11:14:20 +0530

    Update Student.java

--- model/Student.java
+++ model/Student.java
@@ -3,6 +3,7 @@ import  java.util.*;
 public class Student extends Person {
     private String studentId;
     private HashMap<String,Double>grades=new HashMap<>();
+    private List<String>enrolledCourse=new ArrayList<>();
 
     public Student(String name,int age,String studentId){
         super(age, name);
@@ -24,5 +25,16 @@ public class Student extends Person {
 
     public Map<String,Double> getGrades(){
         return grades;
+    }
+
+    public void EnrolledCourse(String courseCode){
+        enrolledCourse.add(courseCode);
+    }
+
+    public  List<String>getEnrolledCourse(){
+        return  enrolledCourse;
+    }
+    public void addGrade(String courseCode,Double grade){
+        grades.put(courseCode,grade);
     }
 }
